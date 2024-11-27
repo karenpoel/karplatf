5f2599b3b5f60941ddb2f6f08f2581aa8f14fb71
karenpoel <karinaelapova05@gmail.com>
2024-11-27 16:02:44 +0300

    prigram

---
+++ karplat/Assets/scripts/camera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public Transform target;
+    public float smooth = 5.0f;
+    public Vector3 offset = new Vector3(0, 2, -5);
+    void Update()
+    {
+        transform.position = Vector3.Lerp (transform.position, target.position + offset, Time.deltaTime * smooth);
+    }
+
+    // Update is called once per frame
+    
+    
+       
+    
+}
